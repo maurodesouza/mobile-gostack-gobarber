6bce91ea6d6fb3581e3e1adb39e75ec4cc03ec69
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-19 20:54:22 -0300

    📒  class 03 - auth routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from '~/routes';
 
 export default function App() {
-  return <Text>Hmm ... Hellow ?</Text>;
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
 }
