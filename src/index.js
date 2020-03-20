bced87384a1ac8644b48a3a6ef368f7a28b4c705
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-20 11:46:31 -0300

    📒  class 08 - config StatusBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from '~/routes';
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
     </NavigationContainer>
   );
 }
