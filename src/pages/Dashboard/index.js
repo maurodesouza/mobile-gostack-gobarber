ff6c16ff8fa7a25e3e7bf3b6b06d4a14579c6d77
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-23 01:28:15 -0300

    📔  class 24 - load appointments

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
 
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
@@ -8,17 +9,19 @@ import api from '~/services/api';
 import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
+  const isFocused = useIsFocused();
+
   const [appointments, setAppointments] = useState([]);
 
+  const loadAppointments = async () => {
+    const { data } = await api.get('/appointments');
+
+    setAppointments(data);
+  };
+
   useEffect(() => {
-    const loadAppointments = async () => {
-      const { data } = await api.get('/appointments');
-
-      setAppointments(data);
-    };
-
-    loadAppointments();
-  }, []);
+    if (isFocused) loadAppointments();
+  }, [isFocused]);
 
   const onCancel = async id => {
     const { data } = await api.delete(`/appointments/${id}`);
